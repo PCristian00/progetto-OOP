b3105658b5c527ccd27f113e2011fd4da3e63770
PCristian00 <95343580+PCristian00@users.noreply.github.com>
2021-12-17 16:28:23 +0100

    Caricamento Progetto Maven

---
+++ pressurestats/src/main/java/it/univpm/pressurestats/PressurestatsApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.pressurestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PressurestatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PressurestatsApplication.class, args);
+	}
+
+}
---
+++ pressurestats/src/test/java/it/univpm/pressurestats/PressurestatsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.pressurestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PressurestatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

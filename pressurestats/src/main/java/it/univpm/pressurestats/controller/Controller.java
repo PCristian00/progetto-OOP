d4567d54901f3c096b97ff18fcd78550398043ca
Diego Settimi <S1098119@studenti.univpm.it>
2021-12-28 17:12:15 +0100

    Controller

--- pressurestats/src/main/java/it/univpm/pressurestats/controller/Controller.java
+++ pressurestats/src/main/java/it/univpm/pressurestats/controller/Controller.java
@@ -1,5 +1,24 @@
 package it.univpm.pressurestats.controller;
-//commento controller  
-public class Controller {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.univpm.pressurestats.service.Service;
+
+//commento controller  
+
+@RestController
+public class Controller {
+	@Autowired
+	Service service;
+	
+	@GetMapping(value="/prova")
+	public ResponseEntity<Object> getForecast(@RequestParam(name = "id", defaultValue = "3173006") String id)
+	{
+		return new ResponseEntity<>(service.getForecast(service.getJSONForecast(id, true)), HttpStatus.OK);
+	}
 }
